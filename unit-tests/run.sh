b883dfd7389f04fd1e49dd521174de28fdd4719b
Nathan McKinley <nmckinley@google.com>
2018-01-26 00:31:59 +0000

    Track master and do rebases where necessary.

--- unit-tests/run.sh
+++ unit-tests/run.sh
@@ -9,7 +9,7 @@ set -x
 
 # Create GOPATH structure
 mkdir -p "${GOPATH}/src/github.com/terraform-providers"
-ln -s "${PWD}/terraform-provider-google" "${GOPATH}/src/github.com/terraform-providers/terraform-provider-google"
+ln -s "${PWD}/terraform-intermediate" "${GOPATH}/src/github.com/terraform-providers/terraform-provider-google"
 
 cd "${GOPATH}/src/github.com/terraform-providers/terraform-provider-google"
 
